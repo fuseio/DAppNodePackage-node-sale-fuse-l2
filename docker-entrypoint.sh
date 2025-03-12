38813cc22e4a740866bd7c21e02fcafb4a5a319c
AliakseiMalyshau <aliaksei.malyshau@protofire.io>
2025-03-12 16:15:58 +0100

    fix: Code updates * Updated Dockerfile base image and Compose file * Updated setup wizard * Updated docker-entrypoint.sh file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,17 @@
 if [ -z "${NETWORK}" ]; then
     echo $(date -u +"%Y-%m-%dT%H:%M:%S.%6NZ") ERROR docker-entrypoint.sh: NETWORK is unset or set to the empty string.
     exit 1
-elif [ -z "${AVAIL_EVM_ADDRESS}" ]; then
-    echo $(date -u +"%Y-%m-%dT%H:%M:%S.%6NZ") ERROR docker-entrypoint.sh: AVAIL_EVM_ADDRESS is unset or set to the empty string.
+elif [ -z "${PRIVATE_KEY}" ]; then
+    echo $(date -u +"%Y-%m-%dT%H:%M:%S.%6NZ") ERROR docker-entrypoint.sh: PRIVATE_KEY is unset or set to the empty string.
+    exit 1
+elif [ -z "${REWARD_COLLECTOR_ADDRESS}" ]; then
+    echo $(date -u +"%Y-%m-%dT%H:%M:%S.%6NZ") ERROR docker-entrypoint.sh: REWARD_COLLECTOR_ADDRESS is unset or set to the empty string.
     exit 1
 else
     # Export default environment variables
     export CHECK_NFT_INTERVAL=${AVAIL_CHECK_NFT_INTERVAL:=300}
     export COMISSION_RATE=${AVAIL_COMISSION_RATE:=5}
     export OPERATOR_NAME=${AVAIL_OPERATOR_NAME:="DAppNode operator"}
-    export REWARD_COLLECTOR_ADDRESS=${AVAIL_REWARD_COLLECTOR_ADDRESS:="0x"}
 
     # Output default environment variable(s) value(s)
     echo $(date -u +"%Y-%m-%dT%H:%M:%S.%6NZ") INFO docker-entrypoint.sh: Network - $NETWORK
@@ -34,10 +36,10 @@ genesis_hash = "DEVTST"
 avail_secret_key = "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice"
 check_nft_endpoint = "https://monitoring.avail.fuse.io/check-nft"
 check_nft_interval = $CHECK_NFT_INTERVAL
-avail_evm_address = "$AVAIL_EVM_ADDRESS"
+private_key = "$PRIVATE_KEY"
 commission_rate = "$COMISSION_RATE"
-operator_name="$OPERATOR_NAME"
-reward_collector_address="$REWARD_COLLECTOR_ADDRESS"
+operator_name = "$OPERATOR_NAME"
+reward_collector_address = "$REWARD_COLLECTOR_ADDRESS"
 EOF
 elif [ $NETWORK == "Ember" ]; then
     echo $(date -u +"%Y-%m-%dT%H:%M:%S.%6NZ") ERROR docker-entrypoint.sh: Ember network is not supported right now. Stay tunned.
